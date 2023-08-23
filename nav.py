8ec3fd961a5305f7b6e21152203ecd402e520d00
Nikhil3137 <132592208+Nikhil3137@users.noreply.github.com>
2023-08-23 14:32:31 +0530

    Update nav.py

--- nav.py
+++ nav.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.title("Multi-Page Streamlit App")
-    navigation = st.sidebar.radio("Navigation", ["Home", "Results", "Analysis", "Models"])
+   NTT DATA = st.sidebar.radio("Navigation", ["Home", "Results", "Analysis", "Models"])
 
     if navigation == "Home":
         st.header("Home")
