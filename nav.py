a2f6320e03bcbd639c1393365e240360204d5a0e
Nikhil3137 <132592208+Nikhil3137@users.noreply.github.com>
2023-08-23 16:50:52 +0530

    Update nav.py

--- nav.py
+++ nav.py
@@ -1,21 +1,37 @@
 import streamlit as st
 
 def main():
-    st.title("Multi-Page Streamlit App")
+    st.title("Custom UI Streamlit App")
     
     # Apply custom CSS styles to the Streamlit app
     st.markdown(
         """
         <style>
-            /* Change font style for the whole app */
+            /* Set custom font for the whole app */
             body {
                 font-family: 'Arial', sans-serif;
             }
-            /* Change font style for the sidebar header */
+            /* Style the title */
+            .title-container {
+                text-align: center;
+                padding: 20px;
+                background-color: #333;
+                color: #fff;
+                border-bottom: 3px solid #555;
+            }
+            /* Style the sidebar */
+            .sidebar .sidebar-content {
+                background-color: #555;
+                color: #fff;
+            }
+            /* Style the sidebar header */
             .sidebar .header {
                 font-family: 'Georgia', serif;
+                color: #f0f0f0;
+                padding-top: 20px;
+                padding-bottom: 10px;
             }
-            /* Change font style and background color for the model dropdown */
+            /* Style the model dropdown */
             .sidebar .selectbox select {
                 font-family: 'Verdana', sans-serif;
                 background-color: #f4f4f4;
@@ -23,19 +39,38 @@ def main():
                 border-radius: 5px;
                 padding: 5px;
                 color: #333;
+                width: 100%;
             }
-            /* Change background color of the sidebar */
-            .sidebar .sidebar-content {
-                background-color: #f0f0f0;
+            /* Style the navigation links */
+            .sidebar .stRadio {
+                margin-top: 15px;
+                margin-left: 10px;
+            }
+            /* Style the main content */
+            .main-container {
+                padding: 20px;
+            }
+            /* Style the custom footer */
+            .footer {
+                text-align: center;
+                padding: 10px;
+                background-color: #333;
+                color: #fff;
             }
         </style>
         """,
         unsafe_allow_html=True
     )
     
+    # Title container
+    st.markdown('<div class="title-container"><h1>Custom UI Streamlit App</h1></div>', unsafe_allow_html=True)
+    
     # Sidebar navigation
     navigation = st.sidebar.radio("Navigation", ["Home", "Results", "Analysis", "Models"])
-
+    
+    # Main content
+    st.markdown('<div class="main-container">', unsafe_allow_html=True)
+    
     if navigation == "Home":
         st.header("Home")
         st.write("This is the home page.")
@@ -52,10 +87,11 @@ def main():
         st.write(f"Result: {result}")
 
     elif navigation == "Models":
-        st.sidebar.header("Model List")
+        st.header("Model List")
+        st.markdown('<div class="header">Model List</div>', unsafe_allow_html=True)
         
         # Create a container for the selectbox
-        model_container = st.sidebar.container()
+        model_container = st.container()
         
         # Place the selectbox in the container
         with model_container:
@@ -72,10 +108,12 @@ def main():
         elif model_option == "Model C":
             st.write("You selected Model C. Here's some information about it.")
             st.write("For more details, visit the [Model C Page](https://yourdomain.com/model_c)")
+    
+    # Close main content container
+    st.markdown('</div>', unsafe_allow_html=True)
 
     # Custom footer
-    st.markdown("---")
-    st.markdown("Contact Us: contact@example.com | Learn more about us at [About Us](https://yourdomain.com/about_us)")
+    st.markdown('<div class="footer">Contact Us: contact@example.com | Learn more about us at [About Us](https://yourdomain.com/about_us)</div>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
