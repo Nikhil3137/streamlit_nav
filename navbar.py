bab9d1bf01bc49b1bbfc31f09a962ee601ac4e88
Nikhil3137 <132592208+Nikhil3137@users.noreply.github.com>
2023-08-23 11:41:47 +0530

    Update navbar.py

--- navbar.py
+++ navbar.py
@@ -1,50 +1,25 @@
 import streamlit as st
 
 def main():
-    navigation_options = ["Home", "Analysis", "Examples"]
-    selected_navigation = st.radio("Navigation", navigation_options)
-
-    navigation_bar = """
-    <style>
-    .navigation-bar {
-        display: flex;
-        justify-content: space-around;
-        background-color: #333;
-        padding: 10px;
-        color: white;
-    }
-    .nav-link {
-        text-decoration: none;
-        color: white;
-    }
-    </style>
-    <div class="navigation-bar">
-        <a class="nav-link" href="?p=home">Home</a>
-        <a class="nav-link" href="?p=analysis">Analysis</a>
-        <a class="nav-link" href="?p=examples">Examples</a>
-    </div>
-    """
-
-    st.markdown(navigation_bar, unsafe_allow_html=True)
-
     st.title("Multi-Page Streamlit App")
+    navigation = st.sidebar.radio("Navigation", ["Home", "Results", "Analysis", "Examples"])
 
-    if selected_navigation == "Home":
+    if navigation == "Home":
         st.header("Home")
         st.write("This is the home page.")
 
-    elif selected_navigation == "Results":
+    elif navigation == "Results":
         st.header("Results List")
         for item in range(25):
             st.write(f"Result {item}")
 
-    elif selected_navigation == "Analysis":
+    elif navigation == "Analysis":
         st.header("Analysis")
         x, y = st.number_input("Input X"), st.number_input("Input Y")
         result = x + y
         st.write(f"Result: {result}")
 
-    elif selected_navigation == "Examples":
+    elif navigation == "Examples":
         st.header("Examples Menu")
         st.write("Select an example.")
 
